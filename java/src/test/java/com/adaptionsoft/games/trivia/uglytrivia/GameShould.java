98e67e43aa3e72d5709a777deb17d44a9fa30795
Nacho Cougil <nacho@codurance.com>
2019-07-02 10:26:40 +0200

    Moved test class to its corresponding package Renamed existing test

--- java/src/test/java/com/adaptionsoft/games/trivia/uglytrivia/GameShould.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/uglytrivia/GameShould.java
@@ -1,4 +1,4 @@
-package com.adaptionsoft.games.trivia;
+package com.adaptionsoft.games.trivia.uglytrivia;
 
 import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.jupiter.api.Test;
@@ -8,17 +8,18 @@ import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class SomeShould {
+class GameShould {
 
 	@Test
-	public void when_game_is_created_something_happens() {
+	public void print_nothing_when_game_is_created() {
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(outputStream);
 		System.setOut(printStream);
+		String emptyString = "";
 
 		Game game = new Game();
 
-		assertEquals("", outputStream.toString());
+		assertEquals(emptyString, outputStream.toString());
 
 	}
 }
