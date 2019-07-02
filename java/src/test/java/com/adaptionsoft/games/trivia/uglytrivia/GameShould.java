2912740eb4df6435b27891f0c62f7b99a6a64e8c
Nacho Cougil <nacho@codurance.com>
2019-07-02 11:39:23 +0200

    Added test for printing the name and number of the player

--- java/src/test/java/com/adaptionsoft/games/trivia/uglytrivia/GameShould.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/uglytrivia/GameShould.java
@@ -1,6 +1,7 @@
 package com.adaptionsoft.games.trivia.uglytrivia;
 
 import com.adaptionsoft.games.uglytrivia.Game;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -10,16 +11,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameShould {
 
-	@Test
-	public void print_nothing_when_game_is_created() {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	private Game game;
+	private ByteArrayOutputStream outputStream;
+
+	@BeforeEach
+	void setUp() {
+		outputStream = new ByteArrayOutputStream();
 		PrintStream printStream = new PrintStream(outputStream);
 		System.setOut(printStream);
-		String emptyString = "";
+		game = new Game();
+	}
 
-		Game game = new Game();
+	@Test
+	public void print_nothing_when_game_is_created() {
+		String emptyString = "";
 
 		assertEquals(emptyString, outputStream.toString());
 
+	}
+
+	@Test
+	void print_the_name_and_player_number_when_is_added() {
+		String playerName = "john";
+
+		game.add(playerName);
+
+		String playerNameAndNumber = "john was added\n" +
+				"They are player number 1\n";
+
+		assertEquals(playerNameAndNumber, outputStream.toString());
 	}
 }
