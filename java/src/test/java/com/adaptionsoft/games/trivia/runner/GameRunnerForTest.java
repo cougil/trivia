fc6b04dc82a441469620c3c178fe356965eedfa8
Nacho Cougil <nacho@codurance.com>
2019-07-07 17:48:54 +0200

    Created subclass for testing GameRunner

---
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerForTest.java
@@ -0,0 +1,4 @@
+package com.adaptionsoft.games.trivia.runner;
+
+public class GameRunnerForTest extends GameRunner {
+}
--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GoldenMaster.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GoldenMaster.java
@@ -23,7 +23,7 @@ public class GoldenMaster {
         PrintStream printStream = new PrintStream(outputStream);
         System.setOut(printStream);
 
-        GameRunner.run( new Random(seed) );
+        new GameRunner().run( new Random(seed) );
 
         return outputStream.toString();
     }
